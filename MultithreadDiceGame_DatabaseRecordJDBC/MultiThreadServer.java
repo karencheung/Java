39825cc921a4f95a9436e771905da830f2d29693
Karen Cheung <kach2360@colorado.edu>
2017-12-18 21:27:19 +0800

    Update MultiThreadServer.java

--- MultithreadDiceGame_DatabaseRecordJDBC/MultiThreadServer.java
+++ MultithreadDiceGame_DatabaseRecordJDBC/MultiThreadServer.java
@@ -1,9 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package DiceGame;
+
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -21,7 +17,7 @@ import javax.swing.JFrame;
 
 /**
  *
- * @author K
+ * @author Karen
  */
 public class MultiThreadServer extends JFrame {
 
